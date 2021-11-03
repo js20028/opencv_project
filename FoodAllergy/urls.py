6a6064d4b3e7d76ac83c03556db96afcaf2b92b2
js20028 <mix2088@nate.com>
2021-11-03 22:43:02 +0900

    django admin page

--- FoodAllergy/urls.py
+++ FoodAllergy/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 
 from . import views
 
+app_name = 'FoodAllergy'
+
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='index'),
+    path('<int:allergy_id>/', views.detail, name='detail'),
+    path('register/', views.allergy_register, name='register'),
+    path('regist/', views.regist, name='regist'),
 ]
